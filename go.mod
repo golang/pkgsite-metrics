b57a61bb7ac1d751ee6fad48c355f57d88473163
Julie Qiu <julie@golang.org>
2023-01-26 00:16:21 -0500

    internal/observe: add package
    Package observe is copied from x/vulndb to use for GCP metrics and
    tracing.
    
    Change-Id: Id9bb0a791c0f2f210f99d96efc8a8168acc85b00
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite-metrics/+/463627
    Run-TryBot: Julie Qiu <julie@golang.org>
    Reviewed-by: Julie Qiu <julieqiu@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- go.mod
+++ go.mod
@@ -3,17 +3,22 @@ module golang.org/x/pkgsite-metrics
 go 1.18
 
 require (
-	cloud.google.com/go/errorreporting v0.3.0
+	cloud.google.com/go/errorreporting v0.1.0
 	cloud.google.com/go/logging v1.6.1
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go v1.0.0
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/metric v0.26.0
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace v1.0.0
 	github.com/client9/misspell v0.3.4
 	github.com/google/go-cmp v0.5.9
 	go.opencensus.io v0.24.0
-	golang.org/x/exp/event v0.0.0-20230125214544-b3c2aaf6208d
+	go.opentelemetry.io/otel v1.4.0
+	go.opentelemetry.io/otel/sdk v1.4.0
+	golang.org/x/exp/event v0.0.0-20220218215828-6cf2b201936e
 	golang.org/x/mod v0.7.0
 	golang.org/x/net v0.5.0
-	golang.org/x/tools v0.5.0
-	honnef.co/go/tools v0.3.3
-	mvdan.cc/unparam v0.0.0-20230125043941-70a0ce6e7b95
+	golang.org/x/tools v0.5.1-0.20230117180257-8aba49bb5ea2
+	honnef.co/go/tools v0.2.2
+	mvdan.cc/unparam v0.0.0-20220926085101-66de63301820
 )
 
 require (
@@ -21,12 +26,20 @@ require (
 	cloud.google.com/go/compute v1.12.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.1 // indirect
 	cloud.google.com/go/longrunning v0.3.0 // indirect
-	github.com/BurntSushi/toml v0.4.1 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	cloud.google.com/go/monitoring v1.8.0 // indirect
+	cloud.google.com/go/trace v1.4.0 // indirect
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/go-logr/logr v1.2.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
-	golang.org/x/exp/typeparams v0.0.0-20220218215828-6cf2b201936e // indirect
+	go.opentelemetry.io/otel/internal/metric v0.27.0 // indirect
+	go.opentelemetry.io/otel/metric v0.27.0 // indirect
+	go.opentelemetry.io/otel/sdk/export/metric v0.26.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v0.26.0 // indirect
+	go.opentelemetry.io/otel/trace v1.4.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
---
+++ internal/observe/observe.go
@@ -0,0 +1,104 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package observe provides metric and tracing support for Go servers.
+// It uses OpenTelemetry and the golang.org/x/exp/events package.
+package observe
+
+import (
+	"context"
+	"net/http"
+
+	"golang.org/x/exp/event"
+	"golang.org/x/pkgsite-metrics/internal/derrors"
+
+	mexporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/metric"
+	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
+	gcppropagator "github.com/GoogleCloudPlatform/opentelemetry-operations-go/propagator"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	eotel "golang.org/x/exp/event/otel"
+)
+
+// An Observer handles tracing and metrics exporting.
+type Observer struct {
+	ctx            context.Context
+	tracerProvider *sdktrace.TracerProvider
+	traceHandler   *eotel.TraceHandler
+	metricHandler  *eotel.MetricHandler
+	propagator     propagation.TextMapPropagator
+
+	// LogHandlerFunc is invoked in [Observer.Observe] to obtain an
+	// [event.Handler] for logging to be added to the [event.Exporter] in
+	// addition to the tracing and metrics handlers.
+	LogHandlerFunc func(*http.Request) event.Handler
+}
+
+// NewObserver creates an Observer.
+// The context is used to flush traces in AfterRequest, so it should be longer-lived
+// than any request context.
+// (We don't want to use the request context because we still want traces even if
+// it is canceled or times out.)
+func NewObserver(ctx context.Context, projectID, serverName string) (_ *Observer, err error) {
+	defer derrors.Wrap(&err, "NewObserver(%q, %q)", projectID, serverName)
+
+	exporter, err := texporter.New(texporter.WithProjectID(projectID))
+	if err != nil {
+		return nil, err
+	}
+	// Create exporter (collector embedded with the exporter).
+	controller, err := mexporter.NewExportPipeline([]mexporter.Option{
+		mexporter.WithProjectID(projectID),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		// Enable tracing if there is no incoming request, or if the incoming
+		// request is sampled.
+		sdktrace.WithSampler(sdktrace.ParentBased(sdktrace.AlwaysSample())),
+		sdktrace.WithBatcher(exporter))
+	return &Observer{
+		ctx:            ctx,
+		tracerProvider: tp,
+		traceHandler:   eotel.NewTraceHandler(tp.Tracer(serverName)),
+		metricHandler:  eotel.NewMetricHandler(controller.Meter(serverName)),
+		// The propagator extracts incoming trace IDs so that we can connect our trace spans
+		// to the incoming ones constructed by Cloud Run.
+		propagator: propagation.NewCompositeTextMapPropagator(
+			propagation.TraceContext{},
+			propagation.Baggage{},
+			gcppropagator.New()),
+	}, nil
+}
+
+// Observe adds metrics and tracing to an http.Handler.
+func (o *Observer) Observe(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var otherHandler event.Handler
+		if o.LogHandlerFunc != nil {
+			otherHandler = o.LogHandlerFunc(r)
+		}
+		exporter := event.NewExporter(eventHandler{o, otherHandler}, nil)
+		ctx := event.WithExporter(r.Context(), exporter)
+		ctx = o.propagator.Extract(ctx, propagation.HeaderCarrier(r.Header))
+		defer o.tracerProvider.ForceFlush(o.ctx)
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+type eventHandler struct {
+	o  *Observer
+	eh event.Handler
+}
+
+// Event implements event.Handler.
+func (h eventHandler) Event(ctx context.Context, ev *event.Event) context.Context {
+	if h.eh != nil {
+		ctx = h.eh.Event(ctx, ev)
+	}
+	ctx = h.o.traceHandler.Event(ctx, ev)
+	return h.o.metricHandler.Event(ctx, ev)
+}
