71aaa4621b8bcd3fa9b0229acde7728c9885ad24
Jonathan Amsterdam <jba@google.com>
2023-02-10 12:37:03 -0500

    internal/scan: remove unused functions
    Change-Id: I62f1067725e19fe3df9c3337fd08ab0cf55e5f6c
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite-metrics/+/467298
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- internal/scan/parse.go
+++ internal/scan/parse.go
@@ -18,43 +18,12 @@ import (
 	"golang.org/x/pkgsite-metrics/internal/version"
 )
 
-func ParseRequiredIntParam(r *http.Request, name string) (int, error) {
-	value := r.FormValue(name)
-	if value == "" {
-		return 0, fmt.Errorf("missing query param %q", name)
-	}
-	return ParseIntParam(value, name)
-}
-
-func ParseOptionalIntParam(r *http.Request, name string, def int) (int, error) {
-	value := r.FormValue(name)
-	if value == "" {
-		return def, nil
-	}
-	return ParseIntParam(value, name)
-}
-
-func ParseIntParam(value, name string) (int, error) {
-	n, err := strconv.Atoi(value)
-	if err != nil {
-		return 0, fmt.Errorf("want integer for %q query param, got %q", name, value)
-	}
-	return n, nil
-}
-
 func ParseOptionalBoolParam(r *http.Request, name string, def bool) (bool, error) {
 	s := r.FormValue(name)
 	if s == "" {
 		return def, nil
 	}
 	return strconv.ParseBool(s)
-}
-
-func ParseMode(r *http.Request) string {
-	const name = "mode"
-	// "" is allowed mode as some endpoints
-	// might equate it with their default mode.
-	return r.FormValue(name)
 }
 
 type ModuleSpec struct {
