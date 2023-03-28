f4a4e3dd8e9d83d8a2953783e5e7fc60aeab9a2a
Zvonimir Pavlinovic <zpavlinovic@google.com>
2023-03-28 15:58:08 -0700

    internal/modules: improve package comment
    Change-Id: I8d683daec97a766bdf836120121ce40b14cb52a0
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite-metrics/+/480019
    Run-TryBot: Zvonimir Pavlinovic <zpavlinovic@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- internal/modules/modules.go
+++ internal/modules/modules.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package modules assists in working with modules.
+// Package modules assists in working with modules, e.g.,
+// downloading a module via a Go proxy client.
 package modules
 
 import (
